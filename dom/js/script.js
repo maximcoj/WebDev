556e6294339d65610084317383a2910bd457fd02
maxim <cojocaru.maxim35@gmail.com>
2020-04-18 17:22:51 +0300

    ДЗ. Простые типы данных

--- dom/js/script.js
+++ dom/js/script.js
@@ -1,1 +1,14 @@
-console.log("You are at " + window.location);
+
+let money = parseInt(prompt("Сколько у вас денег?"))
+
+let howManyApples = parseInt(prompt("Сколько вы купили яблок?"))
+
+let howManyBattons = parseInt(prompt("Сколько вы купили батонов?"))
+
+let applesPrice = howManyApples * parseInt(prompt("Сколько стоит одно яблоко?"))
+
+let battonsPrice = howManyBattons * parseInt(prompt("Сколько стоит один баттон"))
+
+let compare = money > applesPrice + battonsPrice 
+
+document.body.innerHTML = compare
